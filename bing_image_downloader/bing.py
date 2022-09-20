37663861ea28afe20e56e20a8e5a3abe68bb6895
unknown <rtinahubert@gmail.com>
2022-09-20 15:04:33 +0300

    get only links

--- bing_image_downloader/bing.py
+++ bing_image_downloader/bing.py
@@ -26,6 +26,7 @@ class Bing:
         assert type(timeout) == int, "timeout must be integer"
         self.timeout = timeout
 
+        self.all_links = []
         # self.headers = {'User-Agent': 'Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0'}
         self.page_counter = 0
         self.headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) ' 
@@ -53,41 +54,7 @@ class Bing:
                 return ""
 
 
-    def save_image(self, link, file_path):
-        request = urllib.request.Request(link, None, self.headers)
-        image = urllib.request.urlopen(request, timeout=self.timeout).read()
-        if not imghdr.what(None, image):
-            print('[Error]Invalid image, not saving {}\n'.format(link))
-            raise ValueError('Invalid image, not saving {}\n'.format(link))
-        with open(str(file_path), 'wb') as f:
-            f.write(image)
-
-    
-    def download_image(self, link):
-        self.download_count += 1
-        # Get the image link
-        try:
-            path = urllib.parse.urlsplit(link).path
-            filename = posixpath.basename(path).split('?')[0]
-            file_type = filename.split(".")[-1]
-            if file_type.lower() not in ["jpe", "jpeg", "jfif", "exif", "tiff", "gif", "bmp", "png", "webp", "jpg"]:
-                file_type = "jpg"
-                
-            if self.verbose:
-                # Download the image
-                print("[%] Downloading Image #{} from {}".format(self.download_count, link))
-                
-            self.save_image(link, self.output_dir.joinpath("Image_{}.{}".format(
-                str(self.download_count), file_type)))
-            if self.verbose:
-                print("[%] File Downloaded !\n")
-
-        except Exception as e:
-            self.download_count -= 1
-            print("[!] Issue getting: {}\n[!] Error:: {}".format(link, e))
-
-    
-    def run(self):
+    def get_links(self):
         while self.download_count < self.limit:
             if self.verbose:
                 print('\n\n[!!]Indexing page: {}\n'.format(self.page_counter + 1))
@@ -105,11 +72,13 @@ class Bing:
             if self.verbose:
                 print("[%] Indexed {} Images on Page {}.".format(len(links), self.page_counter + 1))
                 print("\n===============================================\n")
-
-            for link in links:
-                if self.download_count < self.limit and link not in self.seen:
-                    self.seen.add(link)
-                    self.download_image(link)
-
+            self.all_links += links
+            # for link in links:
+            #     if self.download_count < self.limit and link not in self.seen:
+            #         self.seen.add(link)
+            #         # self.download_image(link)
+            self.download_count += len(links)
             self.page_counter += 1
-        print("\n\n[%] Done. Downloaded {} images.".format(self.download_count))
+            
+        return self.all_links[:self.limit]
+
